facaeba00d3411b7330727f96efb95be45507699
lawrence_com <aceliuchanghong@gmail.com>
2024-06-18 16:23:21 +0800

    feat: 获取骨架成功

--- test/test_handwrite.py
+++ test/test_handwrite.py
@@ -1,17 +1,6 @@
 import os
-
+from utils.judge_font import get_files
 from utils.util import write_pkl
-
-
-def get_files(folder_path, suffix):
-    jpg_files = []
-    for file_name in os.listdir(folder_path):
-        if file_name.endswith(suffix):
-            file_path = os.path.join(folder_path, file_name)
-            if os.path.isfile(file_path):
-                jpg_files.append(file_path)
-    return jpg_files
-
 
 # 示例用法
 folder_path = '../style_samples'
---
+++ utils/deal_before_generate.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from utils.judge_font import get_files
+
+# 设置基础目录，文件后缀名，保存路径，集数量和展示图片数量
+base_dir = '../style_samples'
+suffix = ".jpg"
+save_pics_path = 'suit_pics'
+set_nums = 10
+show_pics_num = 2
+
+
+def resize_thin_character(pics):
+    """
+    将输入图片进行处理，提取出字符骨架并显示部分结果。
+
+    参数:
+    pics (list): 图片文件路径列表
+    """
+    length = len(pics)
+    index = 0
+    for pic in tqdm(pics, total=length):  # 使用tqdm显示处理进度
+        # 读取图片为灰度图
+        style_img = cv2.imdecode(np.fromfile(pic, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
+        # 调整图片大小为64x64
+        fix = cv2.resize(style_img, (64, 64))
+
+        # 反转颜色，使前景为白色，背景为黑色
+        fix = cv2.bitwise_not(fix)
+
+        # 使用形态学操作获取骨架
+        size = np.size(fix)
+        skel = np.zeros(fix.shape, np.uint8)
+
+        # 二值化处理
+        ret, fix = cv2.threshold(fix, 127, 255, cv2.THRESH_BINARY)
+        # 获取形态学操作的结构元素
+        element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+
+        # 使用循环进行骨架提取
+        while True:
+            open = cv2.morphologyEx(fix, cv2.MORPH_OPEN, element)  # 形态学开操作
+            temp = cv2.subtract(fix, open)  # 获取开操作的差异
+            eroded = cv2.erode(fix, element)  # 腐蚀操作
+            skel = cv2.bitwise_or(skel, temp)  # 合并结果
+            fix = eroded.copy()  # 更新fix
+
+            if cv2.countNonZero(fix) == 0:  # 如果图片中所有像素都为零，跳出循环
+                break
+
+        # 反转骨架图像颜色，使骨架为黑色，背景为白色
+        skel = cv2.bitwise_not(skel)
+
+        # 如果需要展示部分结果图片
+        if show_pics_num > index:
+            plt.imshow(skel, cmap='gray')
+            plt.show()
+        index += 1
+
+
+if __name__ == '__main__':
+    # 获取文件列表
+    files_list = get_files(base_dir, suffix)
+    # 调用函数处理图片
+    resize_thin_character(files_list)
