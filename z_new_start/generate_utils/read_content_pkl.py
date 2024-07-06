4c2af56bca3ad5281dfdc4d58161af5a5bd801ee
lawrence_com <aceliuchanghong@gmail.com>
2024-07-06 17:13:43 +0800

    refactor: png转pkl

--- z_new_start/generate_utils/read_content_pkl.py
+++ z_new_start/generate_utils/read_content_pkl.py
@@ -10,7 +10,7 @@ def main(opt):
         print(item)
         plt.imshow(item['img'], cmap='gray')
         plt.show()
-        if i + 1 > show_num_img:
+        if i + 1 >= show_num_img:
             break
 
 
@@ -18,10 +18,11 @@ if __name__ == '__main__':
     """
     conda activate SDTLog1
     cd z_new_start/generate_utils
-    python read_pkl.py
+    python read_content_pkl.py
+    python read_content_pkl.py --pkl lch_pics_pkl/HYWeiBeiJ.pkl --nums 2
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('--pkl', default='new_chinese_content.pkl', help='读取文件')
-    parser.add_argument('--nums', default=3, type=int, help='展示数量')
+    parser.add_argument('--nums', default=2, type=int, help='展示数量')
     opt = parser.parse_args()
     main(opt)
